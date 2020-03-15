f61f7135822c957135e93953407e7323c1ac9953
Kaede Fujisaki <kaede@hexe.net>
2020-03-16 02:17:56 +0900

    register sidebar

--- functions.php
+++ functions.php
@@ -3,5 +3,17 @@
 function lunar_enqueue_styles() {
   wp_enqueue_style('lunar_style', get_template_directory_uri() . '/style.css', false, filemtime(get_theme_file_path('style.css')), 'all');
 }
-
 add_action( 'wp_enqueue_scripts', 'lunar_enqueue_styles' );
+
+function scrappy_widgets_init() {
+  register_sidebar( array(
+    'id' => 'right-sidebar',
+    'name' => 'Right Sidebar',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside>',
+    'before_title' => '<h2 class="widget-title">',
+    'after_title' => '</h2>'
+    )
+  );
+}
+add_action( 'widgets_init', 'scrappy_widgets_init' );
--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,7 @@
 <div id="secondary" class="widget-area" role="complementary">
-  <?php do_action( 'before_sidebar' ); ?>
-  <?php dynamic_sidebar('right-sidebar'); ?>
-  <?php do_action( 'after_sidebar' ); ?>
+<?php
+  do_action( 'before_sidebar' );
+  dynamic_sidebar('right-sidebar');
+  do_action( 'after_sidebar' );
+?>
 </div>
