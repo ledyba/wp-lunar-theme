9d621d95bc24bae24ce86fa1afafcf3aefde90b0
Kaede Fujisaki <kaede@hexe.net>
2020-03-16 02:25:48 +0900

    add 404

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div id="primary" class="site-content">
+  <div id="content" role="main">
+    <article id="post-0" class="post error404 not-found">
+      <header class="entry-header">
+        <h1 class="entry-title">Page not found!</h1>
+      </header>
+      <div class="entry-content">
+        <p>It looks like nothing was found at this location. Maybe try a search?</p>
+        <?php get_search_form(); ?>
+      </div>
+    </article>
+  </div>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
