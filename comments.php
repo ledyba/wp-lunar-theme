5d2116f1af01d28af84aa280ddecdf02b4795b74
Kaede Fujisaki <kaede@hexe.net>
2020-03-16 03:08:55 +0900

    make shorten

--- comments.php
+++ comments.php
@@ -4,10 +4,6 @@
   <p class="nopassword">This post is password protected. Enter the password to view any comments.</p>
 </div>
 <?php
-    /* Stop the rest of comments.php from being processed,
-     * but don't kill the script entirely -- we still have
-     * to fully load the template.
-     */
     return;
   }
 ?>
