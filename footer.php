16dc1e6e444f08213517c71cfc6fd5abaabf96e7
Kaede Fujisaki <kaede@hexe.net>
2020-03-26 06:47:43 +0900

    add license

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 </div><!-- #main -->
 <footer id="colophon" class="site-footer" role="contentinfo">
   <div class="site-info">
-    <span style="font-style: italic;">
-      Since 2004- / psi &amp; Kaede Fujisaki
-    </div>
+      2004-<?php echo date('Y'); ?> |
+      psi &amp; Kaede Fujisaki |
+      <a style="vertical-align: middle;" rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="クリエイティブ・コモンズ・ライセンス" style="border-width:0;" src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" /></a>
+  </div>
 </footer>
 </div>
 <?php wp_footer(); ?>
