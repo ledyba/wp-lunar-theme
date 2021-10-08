5e895869fcd59996316a461831564adb11658b93
Kaede Fujisaki <kaede@hexe.net>
2021-10-09 00:56:55 +0900

    update

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <footer id="colophon" class="site-footer" role="contentinfo">
   <div class="site-info">
       2004-<?php echo date('Y'); ?> |
-      psi &amp; Kaede Fujisaki |
+      Kaede Fujisaki |
       <a style="vertical-align: middle;" rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="クリエイティブ・コモンズ・ライセンス" style="border-width:0;" src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" /></a>
   </div>
 </footer>
