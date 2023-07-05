d447463f4d18943ce8968d810de5e45ce28c2b9c
Kaede Fujisaki <kaede@hexe.net>
2023-07-06 07:54:32 +0900

    title

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 <div id="lunar-icon"></div>
 <div id="page">
   <?php do_action( 'before' ); ?>
-  <header id="masthead" class="site-header" role="banner">
+  <header id="master-head" class="site-header" role="banner">
     <h1 class="site-title"><a href="<?php echo home_url('/'); ?>" title="<?php echo esc_attr(get_bloginfo('name', 'display')); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
     <blockquote style="font-style: normal;"><?php
       include(dirname(__FILE__).'/kotoba.php/kotoba.php');
